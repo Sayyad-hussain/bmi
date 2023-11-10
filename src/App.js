66d99c4b667e6782559effe75d8c96c3d74f2fc8
sayyad <sayyadhussain315@gmail.com>
2023-11-10 20:31:24 +0500

    bmi calculator react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [weight, setWeight] = useState(" ");
+  const [height, setHeight] = useState(" ");
+  const [bmi, setBmi] = useState(' ')
+  const [message, setMessage] = useState(' ')
+
+  let calcBmi = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (weight === 0 || height === 0) 
+    {
+      alert('Please enter a valid weight and height')
+    } else {
+      let x = height / 100;
+      console.log(height)
+      let m = weight / x;
+      let a = m/x ;
+      let bmi = a;
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+
+      <div className="container">
+      <h2 className='text'>BMI Calculator</h2>
+
+      <form onSubmit={calcBmi}>
+        <div>
+          <label>Weight (kg): </label>
+          <input type="text" value={weight} onChange={(w) => setWeight(w.target.value)}  />
+        </div>
+        
+        <div>
+          <label>Height (cm):  </label>
+          <input type="text" value = {height} onChange={(h) => setHeight(h.target.value)} />
+        </div>
+
+        <div>
+          <button className='btn' type='submit' >Calculate</button>
+          
+        </div>
+        
+
+        <div className="pad">
+          <h1>{`BMI :` + bmi}</h1>
+          <center>
+          <p><b>{message}</b></p>
+          </center>
+          
+        </div>
+        
+
+
+      </form>
+
+        
+      </div>
+
+
+     
+     
+
+
+
     </div>
   );
 }
